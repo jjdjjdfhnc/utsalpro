d3fc51420b136f94d867e8d4c450f1d0b0da4c7c
abubakar basri <114304307+jjdjjdfhnc@users.noreply.github.com>
2022-11-24 11:11:52 +0700

    Create Variabel.java
    uts

---
+++ Variabel.java
@@ -0,0 +1,13 @@
+public class ujian_uts{
+  public static void main(String[]args){
+    String nama = "Abubakar Basri";
+    String tempat_tanggal_lahir = "Saya Lahir di Bogor Tahun 2003";
+    String umur = "19";
+    String sebagai = "mahasiswa";
+      
+    System.out.println(nama);
+    System.out.println(tempat_tanggal_lahir);
+    System.out.println(umur);
+    System.out.println(sebagai);
+  }
+}
